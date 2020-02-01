68b9d9a0eed158cb68d15799c999c0d4d5985873
maminez23 <amineziadi23@gmail.com>
2020-02-01 14:46:09 +0100

    tha last function is not completed

--- warmUp1/warmUp1.js
+++ warmUp1/warmUp1.js
@@ -4,3 +4,47 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+
+x = "belgacem" + "zabbes";
+console.log(x);
+
+function div(x){
+if (x%3===0){
+return(x +"is multiple of 3")
+}
+return(x +"is not multiple of 13")
+}
+
+
+var arr=[13,14,13,15,16,17,19,13,16,15];
+for (var idx in arr){
+        m+=arr[idx]
+        }
+        return m/arr.length;
+        }
+
+
+function indexOf(arr,mon){
+    var x;
+for (var idx in arr){
+if arr[idx]===arr1[0]{
+return idx+1
+}
+}
+
+function ageInSeconds(birth){
+alert("put your birthday in this format jan/01/2000")
+var x = Date();
+var arr1=[];
+var arr2=[];
+arr1=x.split(' ');
+arr2= birth.split('/')
+var month=['Jan','Feb','Mar','Apr','Mai','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+indexOf(arr,arr1[0]);
+
+}
+
+
+
+
+
