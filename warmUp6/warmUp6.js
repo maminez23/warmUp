f4dd39f530ef0142b9eb5de7cc87d61ef01c96ab
maminez23 <amineziadi23@gmail.com>
2020-02-07 14:09:14 +0100

    warmup6

--- warmUp6/warmUp6.js
+++ warmUp6/warmUp6.js
@@ -2,19 +2,42 @@
 // objKey({name:'ahmed',age:20}) ==> ['name','age']
 
 function objKey(obj) {
-  // your code here...
+  var array = [];
+  for( var key in obj) {
+  array.push(key) ;
+  }
+   return array;
 }
 
 // 2 - Write a function named objLength to get the length of a JavaScript object
 // objLength({name:'ahmed',age:20}) ==> 2
 
 function objLength(obj) {
-  // your code here...
+    var array = [];
+    var lengthObj = 0;
+    for( var key in obj) {
+        lengthObj += 1;
+    }
+    return lengthObj;
 }
 
-// 3 - Write a function named objSort to sort an array of JavaScript objects based on the id
-// objSort([{id:2,value:50},{id:0,value:70},{id:1,value:40}]) ==> [{id:0,value:70},{id:1,value:40},{id:2,value:50}]
 
-function objSort(obj) {
-  // your code here...
+// 3 - Write a function named objSort to sort an array of JavaScript objects based on the id
+// objSort([{id:2,value:50},{id:1,value:70},{id:0,value:40}]) ==> [{id:0,value:70},{id:1,value:40},{id:2,value:50}]
+
+// the function compare, starting from index=1, every element with all the precedent elements. Every time id is inferior, it will switch positions of the two values of array
+function objSort(array) {
+  for(var i = 1; i< array.length; i++) {
+     var k = i;
+     for( var j= k-1; j >= 0; j--){
+        if (array[k].id < array[j].id ) {
+
+             var x = array[j];
+             array[j] = array[k];
+             array[k] = x;
+             k -= 1;
+        }
+     }
+  }
+  return array;
 }
