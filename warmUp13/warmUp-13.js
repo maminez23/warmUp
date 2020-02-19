8627edda39cd13edcd26777a82858edb76420484
maminez23 <amineziadi23@gmail.com>
2020-02-19 10:11:16 +0100

    'amineZ'

--- warmUp13/warmUp-13.js
+++ warmUp13/warmUp-13.js
@@ -1,17 +1,56 @@
-// 1-using + operator combine your partner first and last name .
-var str = "Firas" + "Mrad"
-// 2-find if the number 13 is a multiple of 3 or not.
-var x = 13%3; //if x = 0 then 13 is multiple of 3 otherwise it's not
-// 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
-function average(array){
-    var sum = 0;
-    for( var i =0; i < array.length; i++){
-        sum += array[i];
-    }
-return sum/array.length
-}
-// 4-calculate your age in seconds.
-function age(n){
-return n*365*24*60*60;
+//create a function isSubset that should take two arrays and determine whether the second array is a subset of the first array. 
+//For example:  array: [ 1, 2, 3, 4 ], sub:[ 2, 3, 4 ] => true
+//              array: [ 2, 2, 2, 3, 4 ], sub:[ 2, 4, 3 ] => true
+//              array: [ 2, 3, 3, "a" ],  sub:[ 3, 3, 3, "a" ] => false
 
+
+function isSubset(array1, array2){
+
+    var test = true;
+    var test1 = true;
+    var ob = {};
+    var ob2 = {};
+    for(var i = 0; i < array2.length; i++){
+
+        if (!array1.includes(array2[i])){
+            test = false;
+            break
+        }
+        else if (ob[array2[i]] === undefined){
+
+            ob[array2[i]] = 1
+        }
+        else {
+            ob[array2[i]] += 1;
+        }
+    }
+    if(test === false){
+        return test;
+    }
+    else {
+           for(var i = 0; i < array1.length; i++){
+
+               if (ob2[array1[i]] !== undefined){continue}
+               else {
+                 ob2[array1[i]] = 1;
+                 for ( var j = i+1; j < array1.length; j++){
+                   if (array1[j] === array1[i]){
+                     ob2[array1[i]] += 1;
+
+                   }
+                     }
+               }
+           }
+    }
+
+    for( var key in ob){
+        if(ob[key] !== ob2[key]){
+            test1 = false;
+            break
+        }
+    }
+return (test && test1)
 }
+// this function check if all the elements in array2 are in array1 and then verify the occurency of an element in array2 if it's the same or less as the same element in array1
+// otherwise return false
+// so the second part calculate the occurence of the elements in array1 and test it with the array2
